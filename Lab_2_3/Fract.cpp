d371a00996ff994cc8402551b51187108f69df6c
Evgeniy <original.falin@yandex.ru>
2021-03-13 12:14:32 +0300

    Добавьте файлы проекта.

---
+++ Lab_2_3/Fract.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "fract.h"
+
+Fract Fract::operator+(const Fract& other) {
+	Fract temp;
+	temp.denom = this->denom*other.denom;
+	temp.numen = this->denom*other.numen+ other.denom*this->numen;
+	return temp;
+}
+
+std::ostream & operator << (std::ostream & os, const Fract & fr) {
+	os << fr.numen << " / " << fr.denom;
+	return os;
+ }
